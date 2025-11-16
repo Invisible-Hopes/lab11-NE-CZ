2853194607e16b8049670f5ef3485c8bb4b748c4
cz-m550 <chengze0922@gmail.com>
2025-11-16 11:15:49 -0500

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -6,6 +6,26 @@ One function per operation, in order.
 """
 # First example
 def add(a, b): 
+    return a+b
+    
+def sub(a, b):
+    return a-b
+    
+def mul(a, b):
+    return a*b
+
+def div(a, b):
+    if a==0: 
+        raise ZeroDivisionError("Cannot divide by zero")
+    return b/a
+
+def log(a, b):
+    if a<0 or b<=0 or b==1:
+        raise ValueError("Invalid input")
+    return math.log(a, b)
+
+def exp(a, b):
+    return a**b
     pass
 
 
